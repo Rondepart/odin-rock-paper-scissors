d172aab61f3e5510aac2ecb506893488ffc238db
Mark <125941419+Rondepart@users.noreply.github.com>
2026-05-19 04:46:57 +0800

    Add 2 functions for 1 computer and 1 human choice

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    let rndmNum = Math.floor(Math.random() * 3);
+
+    if (rndmNum === 0) {
+        return console.log("rock");
+    } else if (rndmNum === 1) {
+        return console.log("paper");
+    } else if (rndmNum === 2) {
+        return console.log("scissors");
+    }
+
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Enter 0 for rock, 1 for paper, 2 scissors");
+    return console.log(userInput);
+}
+
+getComputerChoice();
+getHumanChoice();
