af79da91f1636da80a0b778b5a78ec3864b05c23
Mark <125941419+Rondepart@users.noreply.github.com>
2026-05-20 02:54:22 +0800

    add playRound function for round logic

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
 console.log("Hello World!");
 
-function getComputerChoice() {
-    let rndmNum = Math.floor(Math.random() * 3);
+const humanScore = 0;
+const computerScore = 0;
 
-    if (rndmNum === 0) {
-        return console.log("rock");
-    } else if (rndmNum === 1) {
-        return console.log("paper");
+function getComputerChoice() {
+    let rndmNum = Math.floor(Math.random() * 3) + 1;
+
+    if (rndmNum === 1) {
+        return "rock";
     } else if (rndmNum === 2) {
-        return console.log("scissors");
+        return "paper";
+    } else if (rndmNum === 3) {
+        return "scissors";
     }
 
 }
 
 function getHumanChoice() {
-    let userInput = prompt("Enter 0 for rock, 1 for paper, 2 scissors");
-    return console.log(userInput);
+    let userInput = prompt("Enter rock, paper, or scissors");
+    return userInput;
 }
 
-getComputerChoice();
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    const normalizedHumanChoice = humanChoice.toLowerCase().replace(/\s+/g, '');
+
+    if (normalizedHumanChoice === computerChoice){
+        return console.log("Tied you and the computer choiced " + computerChoice + ".");
+
+    } else if (normalizedHumanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return console.log("You win, computer choose scissors.");
+
+    } else if (normalizedHumanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return console.log("You lose, computer choose paper.");
+
+    } else if (normalizedHumanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return console.log("You lose, computer choose scissors.");
+
+    } else if (normalizedHumanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return console.log("You win, computer choose rock.");
+
+    } else if (normalizedHumanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return console.log("You lose, computer choose rock.");
+
+    } else if (normalizedHumanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return console.log("You win, computer choose paper.");
+    } else {
+        return console.log("You entered an invalid choice");
+    }
+
+    // return console.log(normalizedHumanChoice + computerChoice);
+} 
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+// getComputerChoice();
+// getHumanChoice();
